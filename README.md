6966a72abadf72ab6a02bb164f9164d10c93fe86
JeffMinsungKim <ziznzkak@gmail.com>
2017-03-20 18:12:36 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# A basic blog tutorial in Django
+> Build a simple blog from the scratch.
+
+## Versions
+> Python 3.5.2 (Release date: 2016-06-27)
+
+> Django 1.9
---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/JeffMinsungKim/.pyenv/versions/3.5.2/envs/django-1.10-tutorial-virtualenv-3.5.2/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
