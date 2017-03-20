79b9a4100825d48eca71dd808862deb9bc7cb03b
JeffMinsungKim <ziznzkak@gmail.com>
2017-03-20 18:25:12 +0900

    A minor change

--- src/posts/views.py
+++ src/posts/views.py
@@ -3,5 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def post_home(request):
-    return HttpResponse("<h1>Hello!</h2>")
+def post_create(request):
+    return HttpResponse("<h1>Create!</h2>")
+
+def post_detail(request): # retrieve
+    return HttpResponse("<h1>Detail!</h2>")
+
+def post_list(request): # list itmes
+    return HttpResponse("<h1>List!</h2>")
+
+def post_update(request):
+    return HttpResponse("<h1>Update!</h2>")
+
+def post_delete(request):
+    return HttpResponse("<h1>Delete!</h2>")
+
